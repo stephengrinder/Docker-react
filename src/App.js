3cc13c2b9e06155edaf7b643b5580a0b4dc4087f
Lohri, Pallavi <Pallavi.Lohri@pearson.com>
2020-01-27 13:55:27 +0530

    change in app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          bye there
+          hi there
         </p>
       </div>
     );
